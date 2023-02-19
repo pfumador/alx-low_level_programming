c0a36f6c7f8eef3cf46c784fd0eacf07c6e3550c
pfumador <paulfumador@gmail.com>
2023-02-19 21:55:46 +0000

    A script that prints a string using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+
+/**
+* main - entry point
+*
+* Description: 'using a puts function to print a string'
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+}
