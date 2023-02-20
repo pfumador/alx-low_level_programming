8d41e96bf9b37a12c344db6ae7f8e0229c70abbd
pfumador <paulfumador@gmail.com>
2023-02-20 10:04:26 +0000

    ma C program that print s a string to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,16 +2,15 @@
 /**
 *main - entry point
 *
-*Description: 'C program that prints and returns value 1 to the standard err*or'
+*Description: 'C program that prints and returns value 1 to the standard error'
 *
 *Return: 1 (Success)
 */
 
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-	
 	return (1);
 }
 
