000599d2f925ebb8bc5c92bcbfc791adf2da4710
pfumador <paulfumador@gmail.com>
2023-02-20 09:51:30 +0000

    a C program that prints a string to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - entry point
+*
+*Description: 'C program that prints and returns value 1 to the standard err*or'
+*
+*Return: 1 (Success)
+*/
+
+int main (void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	
+	return (1);
+}
+
