c139f792f30215ebeab6e8b8268df55bd8a8b220
pfumador <paulfumador@gmail.com>
2023-02-21 07:20:28 +0000

    script that prints both lowercase and uppercase using the 'for' loop and 'putchar' function

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - prints alphabets in lowercase and uppercase
+*followed by a new line
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char letter;
+
+	/* Print lowercase alphabet */
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	/* Print uppercase alphabet */
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	/* Print new line */
+	putchar('\n');
+
+	return (0);
+}
+
