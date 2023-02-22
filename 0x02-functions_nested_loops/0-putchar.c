c7cb0e17c51ec663cad90d99539a543efd70f30e
pfumador <paulfumador@gmail.com>
2023-02-22 05:23:25 +0000

    prints _pu tchar followed by a newa line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,12 @@
 #include main.h
-
 /**
 *main - prints _putchar followed by new line
 *
 *Return: Always 0 (Success)
 */
-
 int main(void)
 {
-	_putchar('_'); 
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('u');
