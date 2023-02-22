859a0af3d9a2ffb1396a234583f0327c95d16a83
pfumador <paulfumador@gmail.com>
2023-02-22 04:41:00 +0000

    all

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
 *main - prints _putchar followed by new line
@@ -8,13 +8,16 @@
 
 int main(void)
 {
-	char *str = "_putchar\n";
-
-	for (int i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-
-	}
+	_putchar('_'); 
+	_putchar('p');
+	_putchar('u');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 
