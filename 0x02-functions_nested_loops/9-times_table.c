a241e2f9718892a4dfc0007c479b4a684f42b64e
pfumador <paulfumador@gmail.com>
2023-02-23 01:35:20 +0000

    prints the 9 times table starting from zero

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,16 +7,33 @@
 */
 void times_table(void)
 {
-	int i;
+	int i, j, k;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(i + '0');
-		_putchar('x');
-		_putchar('9');
-		_putchar('=');
-		_putchar(' ');
-		_putchar((i * 9) + '0');
+		for (j = 0; j < 10; j++)
+		{
+			k = j * i;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');t
+			}
 		_putchar('\n');
 	}
 }
