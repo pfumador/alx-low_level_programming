581ae8f3cb30e6af2c601038b83986111eefdd56
pfumador <paulfumador@gmail.com>
2023-02-25 08:44:24 +0000

    function that prints the numbers, from 0 to 9, followed by a new line but do not print 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_most_numbers - prints the numbers 0 t0 9 without 2 and 4
+*
+*Return: void
+*/
+
+void print_numbers(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9'; && !number 2 || 4; number++)
+	{
+		_putchar(number);
+	}
+
+	_putchar('\n');
+}
