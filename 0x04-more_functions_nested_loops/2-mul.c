e4a66476296d4ac5ecc1e1e9e645b20c48b44816
pfumador <paulfumador@gmail.com>
2023-02-25 07:43:59 +0000

    mu ltiply two integers a and b

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,16 +2,14 @@
 
 /**
 *mul - multiplies two integers
+*
 *@a: first integer
 *@b: second integer
+*
 *Return: product of a and b
 */
 
 int mul(int a, int b)
-{
-	int a;
-	int b;
-	c = a * b;
-	
-	return (c);
+{	
+	return (a * b);
 }
