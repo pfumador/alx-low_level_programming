68cc87c50368b9144e504256a0e060723579fe69
pfumador <paulfumador@gmail.com>
2023-02-25 08:05:05 +0000

    function that prints numbers followed by a new line

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,14 @@
 
 /**
 *print_numbers - function that prints the numbers
-*followed by a new line
 *
-*Return: Always 0 (Success)
 */
 
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 		_putchar("%d", i);
 
 	_putchar("\n");
