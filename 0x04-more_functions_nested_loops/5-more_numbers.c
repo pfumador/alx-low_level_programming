7181778427ced3ef30ce0f276c06fdf567d19d83
pfumador <paulfumador@gmail.com>
2023-02-26 08:55:36 +0000

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,18 +10,17 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; j <=14; j++)
+		for (j = 0; j <= 14; j++)
 			if (j < 10)
 			{
-				_putchar(j + '0');
+				_putchar(j + '0', '1');
 			}
 			else
 			{
-				_putchar('1');
 				_putchar(j - 10 + '0');
 			}
 		_putchar('\n');
 	}
-}
+i}
