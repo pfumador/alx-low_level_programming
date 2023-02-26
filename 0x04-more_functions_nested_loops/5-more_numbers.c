cd71f9a5d515db370b8413d58b012834f1a63617
pfumador <paulfumador@gmail.com>
2023-02-26 09:32:43 +0000

    prints range of numbers multiple times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,8 +8,6 @@
 
 void more_numbers(void)
 {
-	/* i = number of times to print 0 to 14 */
-	 /* j = numbers from 0 to 14 */
 	int i, j;
 
 	for (i = 0; i <= 9; i++)
@@ -17,11 +15,9 @@ void more_numbers(void)
 		for (j = 0; j <= 14; j++)
 		{
 			if (j > 9)
-			{
 				_putchar((j / 10) + '0');
 
 			_putchar((j % 10) + '0');
-			}
 		}
 		_putchar('\n');
 	}
